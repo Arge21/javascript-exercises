1486c2cbed6e708a8d6171974136cdf8135c0968
Arge_21 <115039825+Arge21@users.noreply.github.com>
2025-02-28 16:37:09 -0500

    finish lesson 8 - 11

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(...args) {
+  let sum = 0;
+  if (Array.isArray(args) == true) {
+      flattenArgs = args.flat();
+  } 
+  for (let arg of flattenArgs) sum += arg;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberOne, ...args) {
+  let sum = 0;
+  for (let arg of args) numberOne -= arg;
+  return sum + numberOne;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let sum = 0;
+  flattenArgs = args.flat();
+  for (let arg of flattenArgs) sum += arg;
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  let sum = 1;
+  flattenArgs = args.flat(Infinity);
+  for (let arg of flattenArgs) sum *= arg;
+  return sum;
+};
+
+const power = function(numberOne, power) {
+  return numberOne **= power;
 
 };
 
-const power = function() {
-	
+const factorial = function(number) {
+  let sum = 1;
+  for (i = 1; i <= number; i++) {
+    sum *= i;
+  } return sum;
 };
 
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    num = parseInt(number);
+    if (Math.sign(num) == -1) {
+        return `OOPS`;
+    } else {
+        let sum = 0;
+        let add = 1;
+        for (i = 0; i < num; i++) {
+            sum += add
+            add = sum - add
+        } return sum;
+    }
 };
 
 // Do not edit below this line
